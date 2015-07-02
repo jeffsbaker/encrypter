678bd3c7df266ce153f5db0d05d47ecd06511f64
jeffsbaker <coach12@aol.com>
2015-07-02 14:40:06 -0700

    target-densitydpi=device-dpi

--- viewport.js
+++ viewport.js
@@ -9,7 +9,7 @@
 	
 	For my app I found that to make it look correct on a phone, 7" tablet and 10" tablet
 	I needed different target-densitydpi for each device. A setting of "device-dpi" made
-	it look good on the phone (which was that same as "high-spi") but this made every font
+	it look good on the phone (which was that same as "high-dpi") but this made every font
 	too small on the tablet.  On the 7" tablet I needed "medium-dpi" and on a 10" tablet 
 	I needed "low-dpi".  
 	
@@ -21,21 +21,30 @@ function updateOrientation()
 	if (document.getElementById('footer'))
 		document.getElementById('footer').innerHTML = "";
 	show_viewport();
-	/* Android: screen.width is always the real width in pixels. 
-		For a Nexus 7 in landscape if (window.orientation == 90 || window.orientation == -90)
-			screen.width == 1280
-		In portrait mode screen.width == 800
-		On Android to get the fake width in points: screen.width / window.devicePixelRatio (800 / 1.33 = 602)
-		Problem with above is some Android versions or devices keep screen.width at 800 regardless of orientation
+	/* 
+		Old Android Device using stock Android Browser:
+			Old devices with stock browser do not read the width set in viewport. But we
+			can set a width if we add ", target-densitydpi=device-dpi" to the viewport tag.
+			However, things go strange when we rotate the device. To fix this problem
+			I found that I need to update the viewport on every rotation change depending
+			on the width of the device.
+			
+			To get the standard point width on older Android devices we 
+			need to calculate it with screen.width / window.devicePixelRatio. However on
+			some older devices when you rotate the device screen.width switches to screen.height.
+			So we change the calculation to: point_width = Math.max(screen.width, screen.height);
+		New Android Devices with Chrome Browser:
+			New devices read the viewport width so we don't need to do anything if we detect
+			Chrome in the userAgent.
 		
 		iOS: screen.width is always the virtual width in points. (Points are 2 pixels)
-		For iPhone 5 (Retina display) in landscape
-			screen.width == 320
+		For iPhone 5 (Retina display) 
+		In landscape screen.width == 320
 		In protrait mode screen.width == 320
 		
 	*/
 	var point_width = 0;
-	if (window.cordova) // Only run this command if it is running in a phonegap build app
+	//if (window.cordova) // Only run this command if it is running in a phonegap build app
 	if( /(android)/i.test(navigator.userAgent) )
 	{
 		if (window.orientation == 90 || window.orientation == -90) // landscape
@@ -46,9 +55,9 @@ function updateOrientation()
 		point_width = point_width / window.devicePixelRatio;
 		
 		if (point_width <= 480) // most phones
-			viewport.setAttribute("content", "width=520, user-scalable=no, target-densitydpi=high-dpi, initial-scale=.6");
+			viewport.setAttribute("content", "width=500, user-scalable=no, target-densitydpi=high-dpi");
 		else if (point_width < 800) // most 7" tablets
-			viewport.setAttribute("content", "width=520, user-scalable=no, target-densitydpi=medium-dpi, initial-scale=.7");	
+			viewport.setAttribute("content", "width=520, user-scalable=no, target-densitydpi=medium-dpi");	
 		else if (point_width >= 800) // most 10" tablets
 			viewport.setAttribute("content", "width=520, user-scalable=no, target-densitydpi=low-dpi, initial-scale=1");
 	}
@@ -148,9 +157,10 @@ function show_viewport()
 		document.getElementById('footer').innerHTML += screen.width + "x" +  screen.height + 
 		" " + window.innerWidth + "x" + window.innerHeight +
 		" " + window.devicePixelRatio + " " + window.orientation + " ";
-		/*			screen.width	window.innerWidth	window.devicePixelRatio	
+		/*			screen.width	window.innerWidth	window.devicePixelRatio	Android Version
 			Kindle		1200			600					2
 			Nexus 4		480x800			320x240				1.5
+			Nexus 5		360x592			360x403				3					5
 			Nexus 7		800x1280		602x889				1.33
 			Nexus 10	2560			1280				2
 			HTC Evo		720x1280		360x640				2
@@ -171,10 +181,10 @@ function textbox_focus()
 //document.addEventListener('deviceready', show_viewport, false);
 //show_viewport();
 
-window.addEventListener("orientationchange", updateOrientation, false); // Call when orientation changes
+window.addEventListener("orientationchange", show_viewport, false); // Call when orientation changes
 window.addEventListener( "devicemotion", check_tilt, false ); // call when phone tilted
 //window.addEventListener("resize", updateOrientation); // Call when orientation changes
-updateOrientation(); // Call on first run of app
+show_viewport(); // Call on first run of app
 
 //document.fm.textbox.onclick=textbox_focus;
 //window.scrollTo(0,document.body.scrollHeight);
