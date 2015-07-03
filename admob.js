3051913d5382d021543aefc4aff3ace1ffc40439
jeffsbaker <coach12@aol.com>
2015-07-02 18:06:37 -0700

    Added Interstitial ad

--- admob.js
+++ admob.js
@@ -13,21 +13,21 @@ var admobid = {};
 if( /(android)/i.test(navigator.userAgent) ) { 
     admobid = { // for Android
         banner: 'ca-app-pub-6159883332285612/9159597365',
-        interstitial: ''  
+        interstitial: 'ca-app-pub-6159883332285612/3475893366'  
     };
     if (document.getElementById('footer'))
     	footer.innerHTML = "Android";
 } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
     admobid = { // for iOS
         banner: 'ca-app-pub-6159883332285612/4589796965',
-        interstitial: ''   
+        interstitial: 'ca-app-pub-6159883332285612/4952626562'   
     };
     if (document.getElementById('footer'))
 		footer.innerHTML = "iOS";
 } else {
     admobid = { // for Windows Phone
         banner: 'ca-app-pub-6159883332285612/9019996560',
-        interstitial: ''   
+        interstitial: 'ca-app-pub-6159883332285612/6429359767'   
     };
     if (document.getElementById('footer'))
     	footer.innerHTML = "Other";
@@ -56,5 +56,11 @@ function initApp() {
 
 document.addEventListener('deviceready', initApp, false);
 
-
-
+function admob_interstitial()
+{
+	// preppare and load ad resource in background, e.g. at begining of game level
+	if(AdMob) AdMob.prepareInterstitial( {adId:admobid.interstitial, autoShow:false} );
+	
+	// show the interstitial later, e.g. at end of game level
+	if(AdMob) AdMob.showInterstitial();
+}
